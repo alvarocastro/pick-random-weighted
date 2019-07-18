14e25576f62e54122b64b8758aceff29f5d1543d
Alvaro Castro <moltar89@gmail.com>
2019-07-18 16:32:49 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,13 @@
+export default function (data) {
+	// Split input into two separate arrays of values and weights.
+	const values = data.map(d => d[0]);
+	const weights = data.map(d => d[1]);
+
+	
+	let acc = 0;
+	const sum = weights.reduce((acc, el) => acc + el, 0);
+	const weightsSum = weights.map(el => (acc = el + acc));
+	const rand = Math.random() * sum;
+
+	return values[weightsSum.filter(el => el <= rand).length];
+};
