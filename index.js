05393b7baa72711d668d5faddac19e328b4649d4
Alvaro Castro <moltar89@gmail.com>
2019-07-19 16:30:47 -0300

    Add input validation and support for custom random generation

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 'use strict';
-module.exports = (data) => {
+
+const pick = function (data) {
+	if (!Array.isArray(data)) {
+		throw new TypeError('Expected an Array as the first argument');
+	}
+
 	// Split input into two separate arrays of values and weights.
 	const values = data.map(d => d[0]);
 	const weights = data.map(d => d[1]);
 
-	
 	let acc = 0;
 	const sum = weights.reduce((acc, el) => acc + el, 0);
 	const weightsSum = weights.map(el => (acc = el + acc));
-	const rand = Math.random() * sum;
+	const rand = pick.random() * sum;
 
 	return values[weightsSum.filter(el => el <= rand).length];
 };
+
+pick.random = function () {
+	return Math.random();
+};
+
+module.exports = pick;
