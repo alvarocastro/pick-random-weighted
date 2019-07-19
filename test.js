7b727d191310bac6a10ba0d60b23727f832848da
Alvaro Castro <moltar89@gmail.com>
2019-07-19 16:37:20 -0300

    Add tests

---
+++ test.js
@@ -0,0 +1,42 @@
+const test = require('ava');
+const pick = require('./index.js');
+
+// Mock random number generation.
+let randomSequence = [];
+pick.random = function () {
+	const n = randomSequence.shift();
+	randomSequence.push(n);
+	return n;
+};
+
+test('Should return \'undefined\' with empty lists', t => {
+	t.assert(pick([]) === undefined);
+});
+
+test('Should work with lists of one item', t => {
+	randomSequence = [.1, .6, .3, .4]; // Reset random generation.
+	const data = [
+		['a', 1]
+	];
+
+	t.assert(pick(data) === 'a');
+	t.assert(pick(data) !== 'b');
+});
+
+test('Should work with lists of multiple items', t => {
+	randomSequence = [.1, .6, .3, .4]; // Reset random generation.
+	const data = [
+		['a', 2],
+		['b', 2],
+		['c', 4],
+		['d', 3]
+	];
+
+	t.assert(pick(data) === 'a');
+	t.assert(pick(data) === 'c');
+});
+
+test('Should validate input', t => {
+	t.throws(() => pick(), 'Expected an Array as the first argument');
+	t.throws(() => pick('bad'), 'Expected an Array as the first argument');
+})
